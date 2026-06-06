70eeeb8a59dc51b7b8bb3af6b77f4af9876d694a
HuzS04 <huzaifahshahid797@gmail.com>
2026-06-06 17:21:05 +0100

    Week 1 Day 3 - loops

---
+++ src/LoopPractice.java
@@ -0,0 +1,31 @@
+public class LoopPractice {
+    public static void main(String[] args){
+
+        /*
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+        for(int i = 1; i <= 20; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
+
+        double number = 100;
+
+        while(number >= 1){
+            System.out.println(number);
+            number = number / 2;
+        }
+         */
+
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
