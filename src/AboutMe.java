7ea29dbb974397d03ebeeb7827b7d7ce292eb521
HuzS04 <huzaifahshahid797@gmail.com>
2026-06-06 16:54:29 +0100

    Initial commit - Week 1 Day 1 and 2

---
+++ src/AboutMe.java
@@ -0,0 +1,12 @@
+public class AboutMe {
+    public static void main(String[] args){
+
+        String name = "Huzaifah";
+        int age = 21;
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Student: " + isStudent);
+    }
+}
---
+++ src/GradeChecker.java
@@ -0,0 +1,32 @@
+public class GradeChecker {
+    public static void main(String[] args){
+
+        int score = 60;
+        String grade;
+
+        if(score >= 70){
+            System.out.println("Distinction");
+            grade = "Distinction";
+        } else if (score >= 50) {
+            System.out.println("Pass");
+            grade = "Pass";
+        } else {
+            System.out.println("Fail");
+            grade = "Fail";
+        }
+
+        switch(grade){
+            case "Distinction":
+                System.out.println("Excellent");
+                break;
+            case "Pass":
+                System.out.println("Good effort");
+                break;
+            case "Fail":
+                System.out.println("Keep going");
+                break;
+            default:
+                System.out.println("Unknown grade");
+        }
+    }
+}
