6c99aae8e47263223f03bcef538085a3140100e4
HuzS04 <huzaifahshahid797@gmail.com>
2026-06-06 18:23:42 +0100

    Week 1 Day 5 - arrays

---
+++ src/ArrayPractice.java
@@ -0,0 +1,32 @@
+
+public class ArrayPractice {
+
+    public static int getSum(int[] numbers){
+        int sum = 0;
+        for (int number : numbers) {
+            sum = sum + number;
+        }
+        return sum;
+    }
+
+    public static int getMax(int[] numbers){
+        int max = numbers[0];
+        for (int number : numbers){
+            if (number > max){
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    public static void reverse(int[] numbers){
+        for(int i = numbers.length - 1; i >= 0; i--){
+            System.out.println(numbers[i]);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] num = {1, 2, 3, 4, 5};
+        reverse(num);
+    }
+}
