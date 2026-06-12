5eb9d5ed85f4ae9b94e8d68770b1b9120f41a8d4
HuzS04 <huzaifahshahid797@gmail.com>
2026-06-12 18:11:12 +0100

    Week 2 Day 5 - interfaces and abstract classes

--- src/week2/Main.java
+++ src/week2/Main.java
@@ -2,22 +2,15 @@ package week2;
 
 public class Main {
     public static void main(String[] args){
-        Animal[] animals = {
-                new Dog("Rex", 3),
-                new Cat("Whiskers", 4),
-                new Animal("Generic", 5),
-                new Bird("Feathers", 4)
-        };
+        Circle circle = new Circle("Blue", 5);
+        Rectangle rectangle = new Rectangle("Red", 3, 4);
 
-        for (Animal animal : animals){
-            animal.eat();
-            if(animal instanceof Dog){
-                Dog dog = (Dog) animal;
-                dog.bark();
-            } else if (animal instanceof Bird) {
-                Bird bird = (Bird) animal;
-                bird.fly();
-            }
-        }
+        System.out.println("Area: " + String.format("%.2f", circle.calculateArea()));
+        circle.printColour();
+        circle.describe();
+
+        System.out.println("Area: " + String.format("%.2f", rectangle.calculateArea()));
+        rectangle.printColour();
+        rectangle.describe();
     }
 }
